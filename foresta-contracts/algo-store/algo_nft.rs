6e6df590029b802844e049989a242497f9b09a7f
Tomas Cordero <tomas.cordero.esp@gmail.com>
2024-05-01 13:36:11 -0500

    feat(psp34): algo and execute on psp:34 nft standard

--- foresta-contracts/algo-store/algo_nft.rs
+++ foresta-contracts/algo-store/algo_nft.rs
@@ -1,15 +1,20 @@
 // This contract manages metadata and content identifiers (CIDs) related to algorithms stored as NFTs. 
 // It includes ownership checks, which is crucial for managing access rights.
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 use crate::error::AlgoExecuteError;
 use ink::storage::Mapping;
 use openbrush::{
-    contracts::ownable::*,
+    contracts::{
+        ownable::*,
+        psp34::Id,
+    },
     traits::Storage,
 };
-use proxy::rmrk_proxy::Id;
 
+use openbrush::traits::String;
+
+#[openbrush::implementation(PSP34)]
 #[openbrush::contract]
 pub mod algorithm_nft {
     use super::*;
@@ -18,19 +23,32 @@ pub mod algorithm_nft {
     #[derive(Default, Storage)]
     pub struct AlgorithmNFT {
         #[storage_field]
+        psp34: psp34::Data,
         ownable: ownable::Data,
         metadata: Mapping<Id, String>,
         algorithm_cid: Mapping<Id, String>,
+        token_id: u64,
     }
 
     impl AlgorithmNFT {
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
+                psp34: Default::default(),
                 ownable: Default::default(),
                 metadata: Default::default(),
                 algorithm_cid: Default::default(),
+                token_id: Default::default(),
             }
+        }
+
+        #[ink(message)]
+        pub fn mint(&mut self,to: AccountId, algorithm_cid: String) -> Result<(), AlgoExecuteError> {
+            let mint_id = self.token_id.saturating_add(1);
+            self.token_id = mint_id;
+            self.algorithm_cid.insert(Id::U64(mint_id),&algorithm_cid);
+            let _ = psp34::Internal::_mint_to(self, to, Id::U64(mint_id));
+            Ok(())
         }
 
         #[ink(message)]
--- foresta-contracts/execute/execute_nft.rs
+++ foresta-contracts/execute/execute_nft.rs
@@ -1,33 +1,59 @@
-// Handles the execution permissions associated with NFTs. This is critical for ensuring that only authorized users can execute the 
+// Handles the execution permissions associated with NFTs. This is critical for ensuring that only authorized users can execute the
 // algorithms associated with specific NFTs.
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink::storage::Mapping;
-use proxy::rmrk_proxy::Id;
 use crate::error::AlgoExecuteError;
+use ink::storage::Mapping;
+use openbrush::{
+    contracts::{
+        ownable::*,
+        psp34::Id,
+    },
+    traits::Storage,
+};
 
-
+#[openbrush::implementation(PSP34)]
 #[openbrush::contract]
-pub mod execution_nft {
+pub mod execute_nft {
     use super::*;
 
-    #[derive(Default)]
     #[ink(storage)]
+    #[derive(Default, Storage)]
     pub struct ExecutionNFT {
+        #[storage_field]
+        psp34: psp34::Data,
+        ownable: ownable::Data,
         parent_id: Mapping<Id, Id>,
+        token_id: u64,
     }
 
     impl ExecutionNFT {
         #[ink(constructor)]
-        pub fn default() -> Self {
+        pub fn new() -> Self {
             Self {
+                psp34: Default::default(),
+                ownable: Default::default(),
                 parent_id: Default::default(),
+                token_id: Default::default(),
             }
         }
 
         #[ink(message)]
-        pub fn set_parent_id(&mut self, token_id: Id, parent_id: Id) -> Result<(), AlgoExecuteError> {
+        pub fn mint(&mut self, to: AccountId, parent_id: Id) -> Result<(), AlgoExecuteError> {
+            let mint_id = self.token_id.saturating_add(1);
+            self.token_id = mint_id;
+            self.parent_id.insert(Id::U64(mint_id), &parent_id);
+            let _ = psp34::Internal::_mint_to(self, to, Id::U64(mint_id));
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn set_parent_id(
+            &mut self,
+            token_id: Id,
+            parent_id: Id,
+        ) -> Result<(), AlgoExecuteError> {
+            self.ensure_owner()?;
             self.parent_id.insert(&token_id, &parent_id);
             Ok(())
         }
@@ -36,7 +62,7 @@ pub mod execution_nft {
         pub fn get_parent_id(&self, token_id: Id) -> Option<Id> {
             self.parent_id.get(&token_id)
         }
-        
+
         #[ink(message)]
         pub fn is_valid_execution(&self, token_id: Id, parent_id: Id) -> bool {
             match self.get_parent_id(token_id) {
@@ -44,13 +70,33 @@ pub mod execution_nft {
                 None => false,
             }
         }
-        
-        pub fn verify_execution_rights(&self, exec_id: Id, algo_id: Id) -> Result<(), AlgoExecuteError> {
-            let parent_id = self.parent_id.get(&exec_id).ok_or(AlgoExecuteError::InvalidExecutionNFT)?;
+
+        pub fn verify_execution_rights(
+            &self,
+            exec_id: Id,
+            algo_id: Id,
+        ) -> Result<(), AlgoExecuteError> {
+            let parent_id = self
+                .parent_id
+                .get(&exec_id)
+                .ok_or(AlgoExecuteError::InvalidExecutionNFT)?;
             if parent_id != algo_id {
                 Err(AlgoExecuteError::UnauthorizedAccess)
             } else {
                 Ok(())
+            }
+        }
+
+        fn ensure_owner(&mut self) -> Result<(), AlgoExecuteError> {
+            let owner = self
+                .ownable
+                .owner
+                .get()
+                .ok_or(AlgoExecuteError::NotAuthorized)?;
+            if Some(owner) == Some(Some(self.env().caller())) {
+                Ok(())
+            } else {
+                Err(AlgoExecuteError::NotAuthorized)
             }
         }
     }
